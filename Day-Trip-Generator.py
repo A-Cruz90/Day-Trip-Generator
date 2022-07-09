27ba8a7248de16f791cd650ba0b781b02109656e
Angel Cruz <cruzangel99@gmail.com>
2022-07-09 19:07:26 -0400

    Tested length function for all list

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -7,10 +7,10 @@ entertainment_list =["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"
 restaurant_list =["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
  
 print(destination_list)
-len(destination_list)
+print(len(destination_list))
 print(transportation_list)
-len(transportation_list)
+print(len(transportation_list))
 print(entertainment_list)
-len(entertainment_list)
+print(len(entertainment_list))
 print(restaurant_list)
-len(restaurant_list)
+print(len(restaurant_list))
