26010580af1996fb1a188fe6312ee0b434b2e454
Angel Cruz <cruzangel99@gmail.com>
2022-07-09 18:52:37 -0400

    Generated lists for Destinations, Transport, Entertainment, and Dining

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -1,1 +1,8 @@
+
+
 print("Hello Everyone!")
+
+destination_list["San Juan, PR", "Tokyo, Japan", "Charlotte, NC" "Tampa, FL"]
+transportation_list["Plane", "Car", "Train", "Boat"]
+entertainment_list["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"]
+restaurant_list["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
