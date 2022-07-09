108622cddcb67e21ed9256a8209908843fb1de9a
Angel Cruz <cruzangel99@gmail.com>
2022-07-09 19:03:11 -0400

    Ran Debugg noticed issue with Destination list. Corrected issue

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -1,8 +1,16 @@
-
-
 print("Hello Everyone!")
 
-destination_list["San Juan, PR", "Tokyo, Japan", "Charlotte, NC" "Tampa, FL"]
-transportation_list["Plane", "Car", "Train", "Boat"]
-entertainment_list["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"]
-restaurant_list["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
+
+destination_list =["San Juan, PR", "Tokyo, Japan", "Charlotte, NC", "Tampa, FL"]
+transportation_list =["Plane", "Car", "Train", "Boat"]
+entertainment_list =["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"]
+restaurant_list =["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
+ 
+print(destination_list)
+len(destination_list)
+print(transportation_list)
+len(transportation_list)
+print(entertainment_list)
+len(entertainment_list)
+print(restaurant_list)
+len(restaurant_list)
