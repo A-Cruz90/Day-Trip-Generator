03e2718de37a6fe64037c60c6cb009f7400b1023
Angel Cruz <cruzangel99@gmail.com>
2022-07-10 10:59:54 -0400

    Started woring on code. Beginning with destination

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -1,7 +1,6 @@
 import random
 
 
-print("Hello Everyone, and welcome to the random day generator! We have selected four wonderful locations at random that we know you'll simply adore. Lets begin shall we? ")
 
 
 destination_list =["San Juan, PR", "Tokyo, Japan", "Charlotte, NC", "Tampa, FL"]
@@ -17,24 +16,35 @@ restaurant_list =["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
 
 
 # print(len(destination_list))
-print(random.choice(destination_list))
 # print(len(transportation_list))
-print(random.choice(transportation_list))
+# print(random.choice(transportation_list))
 # print(len(entertainment_list))
-print(random.choice(entertainment_list))
+# print(random.choice(entertainment_list))
 # print(len(restaurant_list))
-print(random.choice(restaurant_list))
-
-
-
-
-
-
+# print(random.choice(restaurant_list))
 
 # print(destination_list)
 # print(transportation_list)
 # print(entertainment_list)
 # print(restaurant_list)
+
+
+
+print( "Hello Everyone, and welcome to the random day generator! We have selected four wonderful locations at random that we know you'll simply adore. Lets begin shall we? ") 
+input(" y or n: ")
+while (True):
+    location = random.choice
+    print("Your randomly selected destination is")
+    print(random.choice(destination_list))
+    print("Will you be ok with that?") 
+    input(" y or n: ")
+    if input == "y":
+        print("Awsome your destination has been selected. ")
+        location = random.choice
+    elif input== "n":
+        print("We are sorry. How about")
+        
+
 
 
 
