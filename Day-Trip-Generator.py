ad309d60e7a8c655103e173211ac8ed9fd7a21e2
Angel Cruz <cruzangel99@gmail.com>
2022-07-10 10:05:27 -0400

    Added the random.choice to all lists

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -1,16 +1,49 @@
-print("Hello Everyone!")
+import random
+
+
+print("Hello Everyone, and welcome to the random day generator! We have selected four wonderful locations at random that we know you'll simply adore. Lets begin shall we? ")
 
 
 destination_list =["San Juan, PR", "Tokyo, Japan", "Charlotte, NC", "Tampa, FL"]
+
 transportation_list =["Plane", "Car", "Train", "Boat"]
+
 entertainment_list =["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"]
+
 restaurant_list =["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
- 
-print(destination_list)
-print(len(destination_list))
-print(transportation_list)
-print(len(transportation_list))
-print(entertainment_list)
-print(len(entertainment_list))
-print(restaurant_list)
-print(len(restaurant_list))
+
+
+
+
+
+# print(len(destination_list))
+print(random.choice(destination_list))
+# print(len(transportation_list))
+print(random.choice(transportation_list))
+# print(len(entertainment_list))
+print(random.choice(entertainment_list))
+# print(len(restaurant_list))
+print(random.choice(restaurant_list))
+
+
+
+
+
+
+
+# print(destination_list)
+# print(transportation_list)
+# print(entertainment_list)
+# print(restaurant_list)
+
+
+
+
+
+
+
+
+
+
+
+
