adeedf59c7c0340136d2012487539b628354285b
Angel Cruz <cruzangel99@gmail.com>
2022-07-13 11:40:24 -0400

    Added coding for Entertainment and Dinning. Need to work on finishing code for repeat if needed

--- Day-Trip-Generator.py
+++ Day-Trip-Generator.py
@@ -3,13 +3,14 @@ import random
 
 
 
-destination_list =["San Juan, PR", "Tokyo, Japan", "Charlotte, NC", "Tampa, FL"]
 
-transportation_list =["Plane", "Car", "Train", "Boat"]
+destination_list =["Las Vegas, NV", "Cleveland, OH", "Charlotte, NC", "Tampa, FL"]
 
-entertainment_list =["Busch Gardens", "Kings Landing", "Touring Fort", "Theater"]
+transportation_list =["Private Bus", "Car Rental", "Cycling", "Uber"]
 
-restaurant_list =["Olive Garden", "Capognas Dugout", "Taco Bell", "Chilli's"]
+entertainment_list =["Busch Gardens", "Kings Landing", "Sight Seeing ", "Theater"]
+
+restaurant_list =["Little NY", "Little Italy", "Taco Bell", "Chilli's"]
 
 
 
@@ -33,28 +34,41 @@ print( "Hello Everyone, and welcome to the random day generator! We have selecte
 print("Your randomly selected destination is")
 
 while (True):
-    d = (random.choice(destination_list))
-    print(d)
+    destination = (random.choice(destination_list))
+    print(destination)
     print("Will you be ok with that?") 
     choice = input("y or n: ")
     if choice == "y":
         print("Awesome your destination has been selected. ")
-        print(d)
         break
     elif choice == "n":
         print("We are sorry. How about:")
 
 
 print("Next we'll select a means for travel while there ")
-print("Your means of travel are by ")
+print("Your means of travel while there will be ")
 while(True):
-    t = (random.choice(transportation_list)) 
-    print(t)
+    trasportation = (random.choice(transportation_list)) 
+    print(trasportation)
     print("Will you be ok with that?") 
     choice = input("y or n: ")
     if choice == "y":
         print("Awesome your means of travel have been selected. ")
-        print(t)
+        break
+    elif choice == "n":
+        print("We are sorry. How about:")
+
+
+
+print("Next we'll decide entertainment while there ")
+print("Your enetertainment will be ")
+while(True):
+    entertainment = (random.choice(entertainment_list)) 
+    print(entertainment)
+    print("Will you be ok with that?") 
+    choice = input("y or n: ")
+    if choice == "y":
+        print("Awesome your entertainment has been selected. ")
         break
     elif choice == "n":
         print("We are sorry. How about:")
@@ -62,11 +76,34 @@ while(True):
 
 
 
-
+print("Next we'll select your dining experience while there ")
+print("Your dining experience will be ")
+while(True):
+    dine = (random.choice(restaurant_list)) 
+    print(dine)
+    print("Will you be ok with that?") 
+    choice = input("y or n: ")
+    if choice == "y":
+        print("Awesome your dining experience has been selected. ")
+        break
+    elif choice == "n":
+        print("We are sorry. How about:")
 
         
 
 
+print("Your day has been planned out to the fullest!")
+print("We will start by arriving at")
+print(destination)
+print("Once there you'll enjoy traveling the location by ")
+print(trasportation)
+print("You'll spend the day doing ")
+print(entertainment)
+print("Lastly we'll end the day with dinner at your local ")
+print(dine)
 
-
+print("We hope you enjoy your day ")
+print("Will you be ok with your perfectly planned out day? ")
+print(input("y or n? "))
+choice = input
 
